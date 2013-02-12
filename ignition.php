a013dc4ea130e275ed4151da45f62f22db2105ab
Ben New <ben@leftclick.com.au>
2013-02-13 02:57:07 +0800

    Improved error handling.

--- ignition.php
+++ ignition.php
@@ -7,13 +7,15 @@
  */
 
 /**
- * Sitegear Ignition main closure.
+ * Sitegear Ignition main closure.  This is the whole ignition sequence, when this is done the website skeleton will be
+ * constructed and the baseline configuration and data files will be in place.
  */
 call_user_func(function() {
 
-	try {
-		echo 'Sitegear Ignition: commencing...', PHP_EOL;
+	echo 'Sitegear Ignition: commencing...', PHP_EOL;
+	$exception = null;
 
+	try {
 		// Resource paths.
 		$resourcesRoot = 'http://sitegear.org/ignition/resources/intermediate/';
 		$localResourcesCache = 'ignition/';
@@ -30,14 +32,16 @@ call_user_func(function() {
 			$url = sprintf('%s%s.php', $resourcesRoot, $requirement);
 			$target = sprintf('%s%s.php', $localResourcesCache, $requirement);
 			// Download the file.
-			echo sprintf('Downloading required script "%s" from "%s" to "%s"...', $requirement, $url, $target), PHP_EOL;
+			echo sprintf('Downloading required script "%s" from "%s" to "%s"...', $requirement, $url, $target);
 			if (!copy($url, $target) || !file_exists($target)) {
 				// We can't continue if all the files don't download.
 				throw new RuntimeException(sprintf('ERROR: Could not copy required script "%s" from source "%s" to local file "%s"; check your internet connection.', $requirement, $url, $target));
 			}
+			// TODO Checksum or some other validation on the script for security.
 			// Create a variable named by $item; assign it the value returned by including the target file.
-			echo sprintf('Including required script "%s"...', $requirement, $target), PHP_EOL;
+			echo 'Executing downloaded script...';
 			$$requirement = require $target;
+			echo 'Done', PHP_EOL;
 		}
 
 		// The three data arrays are defined by the included files.
@@ -51,11 +55,17 @@ call_user_func(function() {
 			// Something is really wrong, one or more of the requirements was downloaded but didn't fulfil its interface.
 			throw new RuntimeException('ERROR: Requirements unfulfilled.');
 		}
-	} catch (Exception $exception) {
-		error_log(sprintf('ERROR: %s', $exception));
+	} catch (Exception $e) {
+		error_log(sprintf('ERROR: %s', $exception = $e));
 	}
 
 	// Cleanup file system.
-	rrmdir($localResourcesCache);
+	if (!rrmdir($localResourcesCache)) {
+		throw new Exception(sprintf('ERROR: Couldn\'t clean up local resources cache "%s"', $localResourcesCache));
+	} elseif (!is_null($exception)) {
+		throw $exception;
+	} else {
+		echo PHP_EOL, 'Done', PHP_EOL, PHP_EOL;
+	}
 
 });
